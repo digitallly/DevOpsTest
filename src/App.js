a89d3976818e567855eef50da8c212c84b7e209f
Sanket Mukherjee <sanketmukherjee189official@gmail.com>
2023-01-23 21:25:05 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. The devops pipeline
-          working perfectly. Testing
+          working perfectly. Testing Done
         </p>
         <a
           className="App-link"
